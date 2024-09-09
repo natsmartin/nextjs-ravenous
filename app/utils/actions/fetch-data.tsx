1e9e89e59b37a55b5ba5a28ba13250ab40cdd821
natsmartin <natsmartin95@gmail.com>
2024-09-09 22:48:30 +0800

    update env

--- app/utils/actions/fetch-data.tsx
+++ app/utils/actions/fetch-data.tsx
@@ -5,7 +5,7 @@ const options = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+        Authorization: `Bearer ${process.env.API_KEY}`
     }
 }
 
