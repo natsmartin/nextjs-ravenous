3100b3f673f90f2c4b54a9c7461f95d4ea36d288
natsmartin <107980240+natsmartin@users.noreply.github.com>
2025-05-28 20:14:08 +0800

    import SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { roboto } from "./fonts/fonts";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Ravenous App",
@@ -20,6 +21,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
