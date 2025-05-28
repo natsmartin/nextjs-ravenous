fe6e26025504deff2cf0f08e714225d515d8fc0b
natsmartin <107980240+natsmartin@users.noreply.github.com>
2025-05-28 18:46:21 +0800

    remove footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 
 import Header from "@components/Header";
-import Footer from "@components/Footer";
+// import Footer from "@components/Footer";
 import Search from "@sections/Search";
 
 
@@ -13,10 +13,10 @@ export default function Home(): React.JSX.Element {
   return (
     <div className="md:flex md:flex-col justify-center text-white w-auto md:w-[auto]">
       <Header />
-      <main className="h-full">
+      <main className="h-full pb-[10vh]">
         <Search />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
